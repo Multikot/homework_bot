8a2514ef2ad9069b476a933c00e86fe53d666b78
Стас Басов <multikot@hotmail.com>
2022-05-03 22:49:54 +0300

    Выполнение домашнего задания - бот-ассистент

--- homework.py
+++ homework.py
@@ -1,82 +1,120 @@
-...
+import logging
+import time
+from http import HTTPStatus
 
-load_dotenv()
+import requests
+import telegram
 
-
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
-
-RETRY_TIME = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
-
-HOMEWORK_STATUSES = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
-}
+from utils_bot import (ENDPOINT, HEADERS, HOMEWORK_STATUSES, PRACTICUM_TOKEN,
+                       RETRY_TIME, TELEGRAM_CHAT_ID, TELEGRAM_TOKEN,
+                       ApiObjectNotFaund, FatalErrorApps, HomeworkStatusError,
+                       MessageNotFound, TokenNotFound, logging_messages_box)
 
 
 def send_message(bot, message):
-    ...
+    """Отправляется сообщение в телеграм.
+    Принимает экземпляр класса, где указан чат id и сообщение.
+    """
+    try:
+        logging.info(logging_messages_box['Send_message'])
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+    except MessageNotFound:
+        logging.error(logging_messages_box['Message_not_found'])
+        raise MessageNotFound(logging_messages_box['Message_not_found'])
 
 
 def get_api_answer(current_timestamp):
+    """Проверяем запрос к Api, статус код запроса должен быть равен 200."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
-    ...
+    try:
+        response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+        if response.status_code != HTTPStatus.OK.value:
+            logging.error(logging_messages_box['Practicum_api_answer_none'])
+            raise ApiObjectNotFaund(
+                logging_messages_box['Practicum_api_answer_none'])
+    except ApiObjectNotFaund(
+            logging_messages_box['Practicum_api_answer_none']):
+        response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    return response.json()
 
 
 def check_response(response):
-
-    ...
+    """Провермяем, что длина словаря больше 0.
+    Проверяем, что в ответ на запрос нам возвращается словарь,
+    что ключ homeworks есть в словаре и что по запросу через ключ
+    возвращается список.
+    """
+    if len(response) == 0:
+        assert False
+    if not isinstance(response, dict):
+        logging.error(logging_messages_box['Type_homework_is_not_dict'])
+        raise TypeError(logging_messages_box['Type_homework_is_not_dict'])
+    if 'homeworks' not in response:
+        logging.error(logging_messages_box['Key_homeworks_not_found'])
+        raise TypeError(logging_messages_box['Type_homework_is_not_list'])
+    if not isinstance(response['homeworks'], list):
+        logging.error(logging_messages_box['Type_homework_is_not_list'])
+        raise TypeError(logging_messages_box['Type_homework_is_not_list'])
+    homework = response['homeworks']
+    return homework
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
-
-    ...
-
-    verdict = ...
-
-    ...
-
+    """Находим имя работы и статус.
+    Если статус успешно получен, то формируем сообщение.
+    """
+    homework_name = homework['homework_name']
+    homework_status = homework['status']
+    try:
+        verdict = HOMEWORK_STATUSES[homework_status]
+    except HomeworkStatusError:
+        logging.error(logging_messages_box['Homework_status_error'])
+        raise HomeworkStatusError(
+            logging_messages_box['Homework_status_error'])
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
-    ...
+    """Проверяем валидность токенов."""
+    if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
+        return True
+    else:
+        if PRACTICUM_TOKEN is None:
+            logging.error(logging_messages_box['Practicum_token_not_found'])
+        if TELEGRAM_TOKEN is None:
+            logging.error(logging_messages_box['Telegram_token_not_found'])
+        if TELEGRAM_CHAT_ID is None:
+            logging.error(logging_messages_box['Telegram_chat_id_not_found'])
+    return False
 
 
 def main():
-    """Основная логика работы бота."""
-
-    ...
-
+    """Основная логика работы бота.
+    Если токены не прошли проверку, сообщаем об этом.
+    Получаем запрос, проверяем ответ на коррекность
+    Получаем элемент домашнего задания и статус, отправляем сообщение.
+    """
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
-
-    ...
-
+    current_timestamp = int(time.time()) - RETRY_TIME
+    if not check_tokens():
+        raise TokenNotFound(logging_messages_box['Token_not_found'])
     while True:
         try:
-            response = ...
-
-            ...
-
-            current_timestamp = ...
+            response = get_api_answer(current_timestamp)
+            homework = check_response(response)
+            message = parse_status(homework)
+            send_message(bot=bot, message=message)
+            current_timestamp = int(time.time())
             time.sleep(RETRY_TIME)
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
+            current_timestamp = int(time.time())
             time.sleep(RETRY_TIME)
         else:
-            ...
+            logging.error(logging_messages_box['Fatal_error_apps'])
+            raise FatalErrorApps(logging_messages_box['Fatal_error_apps'])
 
 
 if __name__ == '__main__':
---
+++ utils_bot.py
@@ -0,0 +1,63 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+from dotenv import load_dotenv
+
+load_dotenv()
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
+handler = RotatingFileHandler(
+    'my_logger.log',
+    maxBytes=50000000,
+    backupCount=5)
+
+PRACTICUM_TOKEN = os.getenv('YANDEX_TOKEN_API')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('MY_CHAT_ID')
+RETRY_TIME = 600
+ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
+HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+
+HOMEWORK_STATUSES = {
+    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
+    'reviewing': 'Работа взята на проверку ревьюером.',
+    'rejected': 'Работа проверена: у ревьюера есть замечания.'
+}
+
+logging_messages_box = {
+    'Send_message': 'Отправлено сообщение',
+    'Practicum_token_not_found': 'Не найден токен Практикума',
+    'Telegram_token_not_found': 'Не найден токен Телеграм-бота',
+    'Telegram_chat_id_not_found': 'Не найден токен телеграм-чата',
+    'Practicum_api_answer_none': 'Ответ API отличный от 200',
+    'Type_homework_is_not_list': 'Тип запроса не является списком',
+    'Key_homeworks_not_found': 'Не найден ключ homeworks',
+    'Type_homework_is_not_dict': 'Тип запроса не является словарем',
+    'Homework_status_error': 'Статус домашней работы не получен',
+    'Message_not_found': 'Не найдено сообщение для отправки',
+    'Token_not_found': 'Один или несколько токенов не найдено',
+    'Fatal_error_apps': 'Сбой в работе приложения'
+}
+
+
+class ApiObjectNotFaund(Exception):
+    pass
+
+
+class HomeworkStatusError(Exception):
+    pass
+
+
+class MessageNotFound(Exception):
+    pass
+
+
+class TokenNotFound(Exception):
+    pass
+
+
+class FatalErrorApps(Exception):
+    pass
